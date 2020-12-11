807e8e4690a4328c22b9062d44cc9d4a0850b48a
Emily Hines <34042849+MrsHines@users.noreply.github.com>
2020-12-11 10:55:31 -0600

    Update crypt.py

--- crypt.py
+++ crypt.py
@@ -4,57 +4,37 @@ def passCheck4(pw):
   guess = ''
   alpha_choices = 'abcdefghijklmnopqrstuvwxyz '
   count = 0
-
-  for i in range(len(alpha_choices)):
-    for j in range(len(alpha_choices)):
-      for k in range(len(alpha_choices)):
-        for l in range(len(alpha_choices)):
-          guess = alpha_choices[i] + alpha_choices[j] + alpha_choices[k] + alpha_choices[l]
-          count = count + 1
-          if guess == pw:
-            return guess, count
+  
+  #Finish the code for this function here
 
 def passCheck5(pw):
   guess = ''
   alpha_choices = 'abcdefghijklmnopqrstuvwxyz '
   count = 0
 
-  for i in range(len(alpha_choices)):
-    for j in range(len(alpha_choices)):
-      for k in range(len(alpha_choices)):
-        for l in range(len(alpha_choices)):
-          for m in range(len(alpha_choices)):
-            guess = alpha_choices[i] + alpha_choices[j] + alpha_choices[k] + alpha_choices[l] + alpha_choices[m]
-            count = count + 1
-            if guess == pw:
+  #Finish the code for this function here.
+  #I left the return statement so that you can see how we return two things at once.
               return guess, count
 
 def passCheckUpper(pw):
   guess = ''
-  alpha_choices = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  alpha_choices = 'abcdefghijklmnopqrstuvwxyz'  #What do you need to do to your list of alphabet choices?
   count = 0
-
-  for i in range(len(alpha_choices)):
-    for j in range(len(alpha_choices)):
-      for k in range(len(alpha_choices)):
-        for l in range(len(alpha_choices)):
-            guess = alpha_choices[i] + alpha_choices[j] + alpha_choices[k] + alpha_choices[l]
-            count = count + 1
-            print(count)
-            if guess == pw:
+  
+  #Finish the code for the function here
+  #I left the return statement so you can see how we return two things at once.
               return guess, count
 
 def subCipher(key, message, encrypt):
   alphabet = "abcdefghijklmnopqrstuvwxyz "
   new_message = ""
-  for let in message:
-    if encrypt:
-      new_message = new_message + key[alphabet.find(let)]
-    else:
-      new_message = new_message + alphabet[key.find(let)]
-  return new_message
+  #Finish the function here
 
 def main():
+  
+  """
+  Some of these functions take longer to run.  You may consider adding print statements into the functions so that you can watch them run.
+  I would only uncomment and run one of the password checkers at a time.  
   
   passw, times = passCheck4("abcd")
   print("Your password is :" + passw)
@@ -68,19 +48,19 @@ def main():
   print("Your password is :" + passw)
   print("It took " + str(times) + " tries to crack it")
 
-  """
+  
   passw, times = passCheckUpper("jDtI")
   print("Your password is :" + passw)
   print("It took " + str(times) + " tries to crack it")
-  """
+  
 
   print(subCipher("_aeioubcdfghjklmnpqrstvwxyz", "hello world", True))
  
   print(subCipher("_aeioubcdfghjklmnpqrstvwxyz", "cohhlzvlphi" , False))
 
   print(subCipher("_aeiouzyxwvtsrqpnmlkjhgfdbc", "hello world", True))
- 
-  #print(subCipher("_aeiouzyxvtsrqpnmlkjhgfdbc", "" , False))
+  
+ """
 
 
 
